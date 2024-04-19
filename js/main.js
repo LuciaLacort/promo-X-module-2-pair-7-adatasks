4e757eba9c8f0c1be84818531cc4809b2faa2413
Pi Rey <pilar.rrdgz@gmail.com>
2024-04-19 11:30:05 +0200

    initial

--- js/main.js
+++ js/main.js
@@ -1,16 +1,34 @@
-'use strict';
+"use strict";
+const tasks = [
+  {
+    name: "Recoger setas en el campo",
+    completed: true,
+  },
+  {
+    name: "Comprar pilas",
+    completed: true,
+  },
+  {
+    name: "Poner una lavadora de blancos",
+    completed: true,
+  },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+  },
+];
 
-const face = document.querySelector('.js-face');
+const taskList = document.querySelector(".task-list");
 
-function handleCLick() {
-    face.innerHTML = ';)';
+//función que pinte el name en la pantalla
+
+function renderName(arr) {
+  for (const item of arr) {
+    console.log(item);
+    for (const j of item.name) {
+      taskList.innerHTML += j;
+    }
+  }
 }
 
-face.addEventListener('click', handleCLick);
-
-
-function handleHover() {
-    face.innerHTML = ':)';
-}
-
-face.addEventListener('mouseout', handleHover);
+renderName(tasks);
