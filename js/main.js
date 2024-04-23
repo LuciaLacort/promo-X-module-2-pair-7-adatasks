0b212d0ac6fdb9ab1c8da9737eb1be0e2cd5847b
Pi Rey <pilar.rrdgz@gmail.com>
2024-04-23 08:12:53 +0200

    rama con id

--- js/main.js
+++ js/main.js
@@ -20,41 +20,69 @@ const tasks = [
 
 const taskList = document.querySelector(".task-list");
 
+//function que me añada un id por objeto
+function addId(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].id = i;
+  }
+  return arr;
+}
+
+addId(tasks);
+console.log(tasks);
+
 //función que pinte el name en la pantalla
 
 function renderName(arr) {
+  taskList.innerHTML = "";
   for (const item of arr) {
-    if (item.completed === true){
-      taskList.innerHTML += `<li class="tachado"><input type="checkbox" checked>${item.name} </li>`;
-
-        } else{ taskList.innerHTML += `<li><input type="checkbox">${item.name} </li>`;
-      console.log(item.name);
+    if (item.completed === true) {
+      //he puesto el id= en el li pero a lo mejor va en el input
+      taskList.innerHTML += `<li class="tachado" id="${item.id}"><input type="checkbox" checked>${item.name} </li>`;
+    } else {
+      taskList.innerHTML += `<li id="${item.id}"><input type="checkbox">${item.name} </li>`;
+      //console.log(item.name);
     }
   }
 }
 renderName(tasks);
 
-
+const paraElConsole = "Dentro de toTheObject: ";
+//function para acceder a un objeto teniendo su id
+function toTheObject(arr, value) {
+  //console.log(paraElConsole + arr[0].id);
+  //console.log(paraElConsole + value);
+  for (let i = 0; i < arr.length; i++){
+    if(value === arr[i].id){
+        console.log('en el if');
+        arr[i].completed = true;
+        console.log(arr[i].completed);
+        //renderName(tasks);
+    }
+  }
+//   for (const cosa of arr) {
+//     console.log(cosa.id);
+//     console.log(value);
+//     if (value === cosa.id) {
+//       cosa.completed = true;
+//     }
+//   }
+}
 
 //Esto lo he dejado comentado porque he empezado a divagar probando cosas de la últimna tarea de hoy, pero prefiero que lo veamos juntas el lunes :)
 
 // const completed = document.querySelector('.completed');
 
-
-
-function handleClick(event){
+function handleClick(event) {
   event.target;
   const eventValue = event.target;
-  const indexTask = tasks.findIndex((task) => {
-    task === eventValue
-  });
-
+  const valueId = eventValue.id;
+  console.log(eventValue.id);
+  toTheObject(tasks, valueId);
+  renderName(tasks);
+  //   const indexTask = tasks.findIndex((task) => {
+  //     task === eventValue;
+  // });
 }
 
-taskList.addEventListener('click', handleClick);
-
-
-
-
-
-
+taskList.addEventListener("click", handleClick);
