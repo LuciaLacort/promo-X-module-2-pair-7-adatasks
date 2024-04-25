274a9aa50a033a10a6d9da8fda2cd77f4887d2ef
Pi Rey <pilar.rrdgz@gmail.com>
2024-04-25 10:44:05 +0200

    add task

--- js/main.js
+++ js/main.js
@@ -1,35 +1,30 @@
 "use strict";
 let tasks = [];
 
-const GITHUB_USER = 'LuciaLacort';
+const GITHUB_USER = "LuciaLacort";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
-
 
 const taskList = document.querySelector(".task-list");
 const counter = document.querySelector(".js-counter");
 
-const getDataApi = () =>{
-fetch(SERVER_URL)
- .then((response) => response.json())
- .then ((info) => {
-  console.log(info);
-  tasks = info.results;
-  renderName(tasks);
-  taskCounter(tasks);
- });
-}
+const getDataApi = () => {
+  fetch(SERVER_URL)
+    .then((response) => response.json())
+    .then((info) => {
+      console.log(info);
+      tasks = info.results;
+      renderName(tasks);
+      taskCounter(tasks);
+    });
+};
 getDataApi();
 
-
-function taskCounter(arr){
-  const completedTasks = tasks.filter(task => task.completed === true);
+function taskCounter(arr) {
+  const completedTasks = tasks.filter((task) => task.completed === true);
   console.log(completedTasks);
-  const notCompletedTasks = tasks.filter(task => task.completed === false);
+  const notCompletedTasks = tasks.filter((task) => task.completed === false);
   counter.innerHTML = `Tienes ${arr.length} tareas. ${completedTasks.length} completadas y ${notCompletedTasks.length} por realizar`;
-};
-
-
-
+}
 
 //function que me añada un id por objeto
 // function addId(arr) {
@@ -78,7 +73,31 @@ function handleClick(event) {
   toTheObject(tasks, eventValue);
   renderName(tasks);
   taskCounter(tasks);
-  console.log(tasks)
+  console.log(tasks);
 }
 
 taskList.addEventListener("click", handleClick);
+
+//Añadir tareas
+
+const newTaskBtn = document.querySelector(".js-btn-add");
+const newTaskArea = document.querySelector(".js-text-task-add");
+
+const handleNewTask = (event) => {
+  event.preventDefault();
+  const inputValue = newTaskArea.value;
+
+  //Idea: hacer un let con la variable del objeto justo anterior del array
+  const newTask = {
+    //id: Aquí coger el valor del ID del objeto anterior que hemos guardado en la variable let y sumarle 1
+    name: inputValue, // sustituye este string vacío por el nombre de la tarea nueva
+    completed: false,
+  };
+  console.log(newTask);
+  // 3. Añade la nueva tarea al array de tareas
+  tasks.push(newTask);
+  // 4. Vuelve a pintar las tareas
+  console.log(tasks);
+  renderName(tasks);
+};
+newTaskBtn.addEventListener("click", handleNewTask);
